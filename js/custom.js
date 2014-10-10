168629f9463b1e12ac0ab1ad859a79b980fb58e1
FiggyD <drew@figcompany.com>
2014-10-10 16:20:08 -0500

    pixel updates per ash's request

--- js/custom.js
+++ js/custom.js
@@ -3,34 +3,34 @@ $(document).ready(function() {
 				 event.preventDefault();
 				$('.searchBox').toggle();
 				$('#background').toggle();
-				$('.headerMainNav li > form').toggleClass('activeArrow');	
+				$('.headerMainNav li > form').toggleClass('activeArrow');
 			});
 			$('#background').click(function(){
 				$('.searchBox').hide();
 				$('#background').hide();
 				$('.headerMainNav li > form').removeClass('activeArrow');
-			});			
+			});
 			$('.submit').click(function(){
 				$('.searchBox').css({'top':'0'});
 			});
 			$('.mobIcon > a').click(function(event){
 				event.preventDefault();
 				$('.moblieNav').slideToggle();
-				
+
 			});
 			$('.locationSelectbox').click(function(){
 				$('.locationDropDown').slideToggle();
-			
+
 			});
 			$('.locationSelectbox1').click(function(){
 				$('.locationDropDown1').slideToggle();
-			
+
 			});
 			$('.locationSelectbox2').click(function(){
 				$('.locationDropDown2').slideToggle();
-			
+
 			});
-			
+
 			/*  Tabs -------------------------------*/
 			$('#tabs-wrapper a').click(function(e) {
 				if ($(this).hasClass('current')){
@@ -42,11 +42,11 @@ $(document).ready(function() {
 
 				$('#tabs-content > div.active').fadeOut(0).removeClass('active');
 				$(id).fadeIn('fast').addClass('active');
-				e.preventDefault(); 
+				e.preventDefault();
 			});
-			
+
 		/* Effects--------------------------------------------*/
-			
+
 			jQuery.easing.def = 'easeOutBounce';
 			// $('<div id="controls">').appendTo('#examples');
 			var sel1 = $("<select>").appendTo('#controls');
@@ -65,7 +65,7 @@ $(document).ready(function() {
 				var method1 = sel1.val();
 				var method2 = sel2.val();
 				$('.example').animate({'top':'0'}, {duration: 50, easing: method1}).animate({'top':'103px'}, {duration: 1000, easing: method2});
-			});	
+			});
 
 			if($('body').width() > 600){
 				jQuery.easing.def = 'easeOutBounce';
@@ -85,10 +85,10 @@ $(document).ready(function() {
 					// var el = $('#' + this.href.split('#')[1]);
 					var method1 = sel1.val();
 					var method2 = sel2.val();
-					$('.example').animate({'top':'0'}, {duration: 50, easing: method1}).animate({'top':'103px'}, {duration: 1000, easing: method2});
-				});	
+					$('.example').animate({'top':'0'}, {duration: 50, easing: method1}).animate({'top':'105px'}, {duration: 1000, easing: method2});
+				});
 			}
-			
+
 			if($('body').width() <= 600){
 				jQuery.easing.def = 'easeOutBounce';
 				// $('<div id="controls">').appendTo('#examples');
@@ -108,6 +108,6 @@ $(document).ready(function() {
 					var method1 = sel1.val();
 					var method2 = sel2.val();
 					$('.example').animate({'top':'0'}, {duration: 50, easing: method1}).animate({'top':'159px'}, {duration: 1000, easing: method2});
-				});	
+				});
 			}
 	});
